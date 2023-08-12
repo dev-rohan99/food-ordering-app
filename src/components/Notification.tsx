9e43c64be97bade5ddee92806142d45356e85d43
rohan-mostofa <rohanmostofa599@gmail.com>
2023-08-12 17:24:19 +0600

    updated

---
+++ src/components/Notification.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Notification = () => {
+
+
+
+    return (
+        <>
+        
+            <div className="h-12 bg-red-500 text-white px-4 flex justify-center items-center text-center text-sm">Free delivery for all order over $50, Order your food now!</div>
+        
+        </>
+    )
+}
+
+export default Notification;
